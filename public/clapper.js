391a5612bdabd3ed5861bb37589e3a2e37e23abe
chrissilich <chrissilich@gmail.com>
2016-11-16 19:19:20 -0500

    clapping, syncing data on board, done. just need to figure out perfect clap event syncing

---
+++ public/clapper.js
@@ -0,0 +1,119 @@
+(function () {
+
+	cookies.nulltoremove = true;
+
+	var $stripes = $(".stripes"),
+		$topStripe = $(".stripes > div").first(),
+		$flash = $(".flash"),		
+		$production = $("input[name='production']").val( cookies("production")?cookies("production"):"" ),
+		$director = $("input[name='director']").val( cookies("director")?cookies("director"):"" ),
+		$camera = $("input[name='camera']").val( cookies("camera")?cookies("camera"):"A" ),
+		$scene = $("input[name='scene']").val( cookies("scene")?cookies("scene"):"1" ),
+		$take = $("input[name='take']").val( cookies("take")?cookies("take"):"1" ),
+		$time = $("input[name='time']"),
+		$date = $("input[name='date']"),
+		$room = $("input[name='room']").val( cookies("room")?cookies("room"):"" ),
+		$syncable = $("input.syncable"),
+		$cookieable = $("input.cookieable"),
+		socket = io();
+		
+
+	// set initial values
+	$date.val( moment().format("YYYY-MM-DD") );
+	var updateTime = function() {
+		$time.val( moment().format("HH:MM:SS.SSS") );
+		requestAnimationFrame(updateTime);
+	}
+	requestAnimationFrame(updateTime);
+	
+
+	var doRoomChange = function(){
+		if (cookies("room") && cookies("room") != $room.val()) { // new entry doesn't match
+			socket.emit("leave-room", cookies("room"));
+			console.log("left room", cookies("room"));
+		}
+		cookies({ room: $room.val() });
+		if ($room.val().length) {			
+			socket.emit("join-room", $room.val());
+			console.log("joined room", $room.val());
+		}
+	}
+	$room.on("keyup", doRoomChange);
+	doRoomChange()
+
+
+	var doSync = function(){
+		var name = $(this).attr("name");
+		var val = $(this).val();
+		
+		console.log("sync", name, val);
+
+		socket.emit("sync", {name: name, val: val, room: cookies("room"), id: socket.id});
+
+	}
+	$syncable.on("keyup", doSync);
+
+
+	var doCookie = function(){
+		var name = $(this).attr("name");
+		var val = $(this).val();
+
+		var cookie = {};
+		cookie[name] = val;
+		console.log("save cookie", cookie);
+		cookies(cookie);
+
+	}
+	$cookieable.on("keyup", doCookie);
+
+
+	socket.on("sync-down", function(data) {
+		if (data.id == socket.id) return; // kill self-updating
+		console.log(data, $("input.syncable[name='"+data.name+"']"), data.val);
+		$("input.syncable[name='"+data.name+"']").val(data.val);
+	})
+
+
+	// audio and animation logic
+	var clapSound = new Howl({
+		src: ['assets/audio/clap.mp3'],
+		onplay: function() {
+			console.log("clap sound play")
+			TweenMax.fromTo($topStripe, 1, {rotation: -45}, {rotation: 0, ease: Linear.easeNone, onComplete: function(){
+				TweenMax.fromTo($flash, 1, {autoAlpha: 1}, {autoAlpha: 0, ease: Linear.easeNone});
+			}})
+		}
+	})
+	
+	$stripes.on("click", function() {
+		clapSound.play();
+	})
+
+})();
+
+
+
+(function() {
+    var lastTime = 0;
+    var vendors = ['webkit', 'moz'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame =
+          window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express')
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static('public'))
+
+// app.get('/', function(req, res){
+// 	res.send('<h1>Hello world</h1>');
+// });
+
+http.listen(3000, function(){
+	console.log('listening on *:3000');
+});
+
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+
+	socket.on("join-room", function(room) {
+		console.log("a user joined", room);
+		socket.join(room);
+	})
+	socket.on("leave-room", function(room) {
+		console.log("a user left", room);
+		socket.leave(room);
+	})
+
+	socket.on("sync", function(data) {
+		console.log("sync", data);
+		io.to(data.room).emit("sync-down", data);
+	})
+
+	socket.on('disconnect', function(){
+		console.log('a user disconnected');
+	});	
+});
